93a9201db159628d6eb83660e87e27d8545d613a
Pkothale <prashantkothale1999@gmail.com>
2025-08-17 11:52:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-btach-28123"
+    bucket = "young-minds-app-btach-281234"
     key = "main"
     region = "ap-southeast-2"
     dynamodb_table  = "my-dynamodb-table"
