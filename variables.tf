708449944931bfd33fd53921566ff8d921ee1d5a
Pkothale <prashantkothale1999@gmail.com>
2025-08-17 11:54:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "ap-southeast-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jenkins-ap-south-1" 
+    default     =  "jenkins-server" 
 }
 
 variable "instance_type" { 
